256a30e076a5fbf0fefaa2f2b32180ad9357fb4c
Tomasz Podsiadlik <tpodsiadlik@future-processing.com>
2020-03-19 09:12:47 +0100

    Post mongoose model

---
+++ models/post.model.js
@@ -0,0 +1,55 @@
+const  mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    likes: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now()
+            }
+
+        }
+    ]
+});
+
+module.exports = Post = mongoose.model("Post", PostSchema);
