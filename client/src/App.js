d7f4d72591c397e90d53690a2fbaeda64b6e0857
Tomasz Podsiadlik <tpodsiadlik@future-processing.com>
2020-03-20 14:50:00 +0100

    Add basic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar.js';
+import Landing from './components/layout/Landing.js';
+
+
 import './App.css';
 
 const App = () => (
     <>
-      <h1>App</h1>
+      <Navbar />
+      <Landing />
     </>
   )
 
