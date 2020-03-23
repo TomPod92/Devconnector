6ba4f445f3475ced7087e4129b1eadd529e473c2
Tomasz Podsiadlik <tpodsiadlik@future-processing.com>
2020-03-23 17:44:09 +0100

    Basic nav routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar.js';
 import Landing from './components/layout/Landing.js';
+import Login from './components/auth/Login.js';
+import Register from './components/auth/Register.js';
 
 
 import './App.css';
 
 const App = () => (
-    <>
-      <Navbar />
-      <Landing />
-    </>
+      <BrowserRouter>
+      
+        <Navbar />
+        <Route exact path="/" component={Landing}/>
+
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+
+      </BrowserRouter>
   )
 
 
