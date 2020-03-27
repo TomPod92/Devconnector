535dcb9038833f5f80bbb7aa4884c3f0efe6e0e9
Tomasz Podsiadlik <tpodsiadlik@future-processing.com>
2020-03-27 12:58:28 +0100

    Add education

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import PrivateRoute from './components/routing/PrivateRoute.js';
 import CreateProfile from './components/profile-forms/CreateProfile.js';
 import EditProfile from './components/profile-forms/EditProfile.js';
 import AddExperience from './components/profile-forms/AddExperience.js';
+import AddEducation from './components/profile-forms/AddEducation.js';
 
 // Redux imports
 import { Provider } from 'react-redux';
@@ -46,6 +47,7 @@ const App = () => {
             <PrivateRoute exact path="/create-profile" component={CreateProfile} />
             <PrivateRoute exact path="/edit-profile" component={EditProfile} />
             <PrivateRoute exact path="/add-experience" component={AddExperience} />
+            <PrivateRoute exact path="/add-education" component={AddEducation} />
           </Switch>
         </section>
 
---
+++ frontend/src/components/profile-forms/AddEducation.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { addEducation } from '../../redux/actions/profile.actions.js';
+
+const AddEducation = (props) => {
+
+    const [ formData, setFormData ] = useState({
+        school: '',
+        degree: '',
+        fieldofstudy: '',
+        from: '',
+        to: '',
+        current: false,
+        description: ''
+    });
+
+    const [toDateDisabled, setToDateDisabled] = useState(false);
+
+    const { school, degree, fieldofstudy, from, to, current, description } = formData;
+
+    const handleInputChange = (event) => setFormData({...formData, [event.target.name]: event.target.value});
+
+    const handleCheckboxChange = () => {
+        setFormData({...formData, current: !current});
+        setToDateDisabled(!toDateDisabled);
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        props.addEducation(formData, props.history);
+    };
+
+    return ( 
+        <>
+            <h1 className="large text-primary">Add Yuor Education</h1>
+
+            <p className="lead"><i className="fas fa-code-branch"></i> Add any school that you have attended</p>
+
+            <small>* = required field</small>
+
+            <form className="form" onSubmit={handleFormSubmit}>
+                <div className="form-group">
+                    <input type="text" placeholder="* School" name="school" value={school} onChange={handleInputChange} required />
+                </div>
+
+                <div className="form-group">
+                    <input type="text" placeholder="* Degree" name="degree" value={degree} onChange={handleInputChange} required />
+                </div>
+
+                <div className="form-group">
+                    <input type="text" placeholder="Field of Study" name="fieldofstudy" value={fieldofstudy} onChange={handleInputChange}/>
+                </div>
+
+                <div className="form-group">
+                    <h4>From Date</h4>
+                    <input type="date" name="from" value={from} onChange={handleInputChange}/>
+                </div>
+
+                <div className="form-group">
+                    <p><input type="checkbox" name="current" checked={current} value={current} onChange={handleCheckboxChange} />{' '}Current Job</p>
+                </div>
+
+                <div className="form-group">
+                    <h4>To Date</h4>
+                    <input type="date" name="to"  value={to} onChange={handleInputChange} disabled={toDateDisabled ? 'disabled' : ''}/>
+                </div>
+
+                <div className="form-group">
+                    <textarea name="description" cols="30" rows="5" placeholder="Program Description" value={description} onChange={handleInputChange}></textarea>
+                </div>
+
+                <input type="submit" className="btn btn-primary my-1" />
+                <Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
+            </form>
+        </>
+    );
+}
+ 
+export default connect(null, { addEducation })(withRouter(AddEducation));
